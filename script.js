f8ebe1ede7e384c50fd7df160f3846ce702e81de
Ishaan911 <123613604+Ishaan911@users.noreply.github.com>
2026-02-03 12:01:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const message = document.getElementById("message");
-const box = document.querySelector(".buttons");
+const box = document.querySelector(".playground");
 
 noBtn.addEventListener("mouseenter", () => {
-    const boxWidth = box.clientWidth;
-    const boxHeight = box.clientHeight;
-
-    const btnWidth = noBtn.offsetWidth;
-    const btnHeight = noBtn.offsetHeight;
-
-    const maxX = boxWidth - btnWidth;
-    const maxY = boxHeight - btnHeight;
+    const maxX = box.clientWidth - noBtn.offsetWidth;
+    const maxY = box.clientHeight - noBtn.offsetHeight;
 
     const x = Math.random() * maxX;
     const y = Math.random() * maxY;
 
-    noBtn.style.left = `${x}px`;
-    noBtn.style.top = `${y}px`;
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
+});
+
+yesBtn.addEventListener("click", () => {
+    message.style.display = "block";
 });
